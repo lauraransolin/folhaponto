2d6c7f1d6b3e0169998739f44c839b87193bae5e
laura <laurabaldanransolin2010@gmail.com>
2026-07-06 13:02:41 -0300

    second commit

---
+++ felizaniversario.php
@@ -0,0 +1,66 @@
+<?php
+echo "<h1>🎉 Feliz Aniversário Prof. Egberto 🎉</h1>";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Aniversário</title>
+
+<style>
+
+body{
+    background:#ffe6f2;
+    text-align:center;
+    font-family:Arial;
+}
+
+h1{
+    color:#d10068;
+    margin-top:30px;
+}
+
+.bolo{
+    width:200px;
+    margin:40px auto;
+}
+
+.velas{
+    font-size:30px;
+}
+
+.topo{
+    width:160px;
+    height:30px;
+    background:pink;
+    margin:auto;
+    border-radius:10px 10px 0 0;
+}
+
+.base{
+    width:200px;
+    height:100px;
+    background:#8B4513;
+    margin:auto;
+    border-radius:0 0 15px 15px;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<div class="bolo">
+
+    <div class="velas">🕯️🕯️🕯️</div>
+
+    <div class="topo"></div>
+
+    <div class="base"></div>
+
+</div>
+
+</body>
+</html>
